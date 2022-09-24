de03d4e0c70bd896be56b60bfa75feeb588a3b48
suhanabolt <106386090+suhanabolt@users.noreply.github.com>
2022-09-24 16:53:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function setup() {
+    video = createCapture(VIDEO)
+    video.size(550,500)
+
+    canvas = createCanvas(500,490)
+    canvas.position(800,70)
+
+    poseNet=ml5.poseNet(video,modelLoaded)
+    poseNet.on("pose",gotPoses)
+}
+
+function draw() {
+    background("#fdbe40")
+}
+function modelLoaded() {
+    console.log("PoseNet Is Initialized!")
+}
+function gotPoses(results) 
+{
+    if(results.length>0)
+    {
+    console.log(results)
+    }
+}
